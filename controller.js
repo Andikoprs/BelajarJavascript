b366a510675008f408f230aeef9fae9f5e3df3fa
Andikoprs <andiko567@gmail.com>
2023-02-21 13:43:22 +0700

    controller-route

--- controller.js
+++ controller.js
@@ -17,6 +17,52 @@ async function getAllUsers(req, res) {
   res.json(dataUsers);
 }
 
+async function addUser(req, res) {
+  console.log(req.body);
+  dataUsers.push(req.body);
+  res.json(dataUsers);
+}
+
+async function updateData(req, res) {
+  const updateDataUserIndex = dataUsers.findIndex((eachData) => {
+    if (eachData.id === req.params.id) {
+      return eachData;
+    }
+  });
+  dataUsers[updateDataUserIndex].nickName = req.body.nickName;
+  res.json(dataUsers);
+}
+
+async function deleteData(req, res) {
+  const dataUserIndex = dataUsers.findIndex((eachData) => {
+    if (eachData.id === req.params.id) {
+      return eachData;
+    }
+  });
+  dataUsers.splice(dataUserIndex, 1);
+  res.json(dataUsers);
+}
+
+async function getAllNickNames(req, res) {
+  const getAllNickNames = dataUsers.map((eachData) => eachData.nickName);
+  console.log(getAllNickNames);
+  res.json(getAllNickNames);
+}
+
+async function getDataById(req, res) {
+  const getId = dataUsers.find((eachData) => {
+    if (eachData.id === req.params.id) {
+      return eachData;
+    }
+  });
+  res.json(getId);
+}
+
 module.exports = {
   getAllUsers,
+  addUser,
+  deleteData,
+  updateData,
+  getAllNickNames,
+  getDataById,
 };
--- index.js
+++ index.js
@@ -3,75 +3,23 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const controller = require('./controller')
-
-const dataUsers = [
-  {
-    nickName: "Sasa",
-    id: "1",
-  },
-  {
-    nickName: "Susu",
-    id: "2",
-  },
-  {
-    nickName: "Sisi",
-    id: "3",
-  },
-];
-
-app.get("/", (req, res) => {
-  res.json({
-    nickName: "Andikopts",
-  });
-});
+const controller = require("./controller");
 
 app.get("/users", controller.getAllUsers);
 
 //api insert
-app.post("/users", (req, res) => {
-  console.log(req.body);
-  dataUsers.push(req.body);
-  res.json(dataUsers);
-});
+app.post("/users", controller.addUser);
 
 //api update
-app.put("/users/:id", (req, res) => {
-  const updateDataUserIndex = dataUsers.findIndex((eachData) => {
-    if (eachData.id === req.params.id) {
-      return eachData;
-    }
-  });
-  dataUsers[updateDataUserIndex].nickName = req.body.nickName;
-  res.json(dataUsers);
-});
+app.put("/users/:id", controller.updateData);
 
 //api delete
-app.delete("/users/:id", (req, res) => {
-  const dataUserIndex = dataUsers.findIndex((eachData) => {
-    if (eachData.id === req.params.id) {
-      return eachData;
-    }
-  });
-  dataUsers.splice(dataUserIndex, 1);
-  res.json(dataUsers);
-});
+app.delete("/users/:id", controller.deleteData);
 
 //api get
-app.get("/users/nickNames", (req, res) => {
-  const getAllNickNames = dataUsers.map((eachData) => eachData.nickName);
-  console.log(getAllNickNames);
-  res.json(getAllNickNames);
-});
+app.get("/users/nickNames", controller.getAllNickNames);
 
-app.get("/users/:id", (req, res) => {
-  const getId = dataUsers.find((eachData) => {
-    if (eachData.id === req.params.id) {
-      return eachData;
-    }
-  });
-  res.json(getId);
-});
+app.get("/users/:id", controller.getDataById);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
